b7e013d1eb2f3860759f31003ff8caf64853039d
AlexTK2012 <121603651@qq.com>
2018-11-12 16:18:33 +0800

    添加目标 添加HKU portal 登陆代码，参考workshop

---
+++ app/src/main/java/cs/hku/group14/schedule/Goal.java
@@ -0,0 +1,14 @@
+package cs.hku.group14.schedule;
+
+/*
+Smart Phone 目标：
+1、预设数据库数据：课程编号-课程时间，线程后台查询
+2、HKU Portal账号登陆，参考WorkShop1，解析 moodle页面
+3、匹配课程编号，展示课程时间为课程表，使用Git 控件
+4、提前一段时间，进行消息提示，可以添加Email 提示。
+ */
+
+
+public class Goal {
+
+}
